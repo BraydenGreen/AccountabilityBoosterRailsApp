72cb3ae7086462d9105b91fe446034d51d60e623
Brayden Green <brayden.p.green@gmail.com>
2018-07-17 17:18:05 -0600

    Adds users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
